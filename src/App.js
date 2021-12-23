6818becff086421387468135101d1eafa43975d8
Anderson Ribeiro <eu@andersonribeiro.dev>
2021-12-23 01:51:56 +0000

    Implemented routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="shop/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
